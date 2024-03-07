505c6d58634c644cf2745436d68ccf290731f8f5
Sirivarakul <sirivarakuljiropakarn@gmail.com>
2024-03-07 22:56:53 +0700

    Setup page directories

---
+++ businage/src/app/(monetary)/monetary-expense-sale/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ExpenseSale() {
+  return (
+    <div>ExpenseSale</div>
+  )
+}
+
+export default ExpenseSale
---
+++ businage/src/app/(monetary)/monetary-tradein/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TradeIn() {
+  return (
+    <div>TradeIn</div>
+  )
+}
+
+export default TradeIn
