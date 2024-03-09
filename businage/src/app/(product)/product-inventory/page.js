e71bac6fde763a5d2e923afcf8630ad44d03b1f7
marklileo <phornphatmak1112@gmail.com>
2024-03-10 00:25:27 +0700

    จัดระเบียบ NextUI
    + Setup NextUI in tailwind.config
    
    ! The problem is on "ROOT COMPONENT"

--- businage/src/app/(product)/product-inventory/page.js
+++ businage/src/app/(product)/product-inventory/page.js
@@ -1,9 +1,106 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Spinner,
+} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
+import { useAsyncList } from "@react-stately/data";
+
+/*  Product stock table
+
+1. Item name (Can edit)
+
+2. Sell price (Can edit)
+
+3. Avg. cost (Non-editable, avg. computed from expenses)
+
+4. Brand (Can edit)
+
+5. Quantity
+
+6. Selling status (Can edit)
+
+7. (ขายอยู่ or not?)
+
+8. Latest updated at ...
+*/
+const rowData = [
+  {
+    key: "1",
+    name: "Tony กระด้าก",
+    role: "CEO",
+    status: "ตายห่า",
+  },
+  {
+    key: "2",
+    name: "Wong",
+    role: "Santum lead",
+    status: "Alive",
+  },
+  {
+    key: "3",
+    name: "Professor X",
+    role: "Mai ru",
+    status: "ตาย",
+  },
+  {
+    key: "4",
+    name: "Professor Y",
+    role: "Mai ru",
+    status: "ตาย",
+  },
+];
+
+const colData = [
+  {
+    key: "name",
+    label: "Name",
+  },
+  {
+    key: "role",
+    label: "Role",
+  },
+  {
+    key: "status",
+    label: "Status",
+  },
+];
 
 function Inventory() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div>Inventory</div>
-  )
+    <NextUIProvider>
+      <Table isStriped>
+        <TableHeader columns={colData}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          items={rowData}
+          emptyContent={"No Product data."}
+          isLoading={isLoading}
+          loadingContent={<Spinner label="Loading Product Stock..." />}
+        >
+          {(row) => (
+            <TableRow key={row.key}>
+              {(columnKey) => (
+                <TableCell>{getKeyValue(row, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </NextUIProvider>
+  );
 }
 
-export default Inventory
+export default Inventory;
--- businage/src/app/page.js
+++ businage/src/app/page.js
@@ -1,17 +1,19 @@
-'use client'
+"use client";
 
 import { permanentRedirect } from "next/navigation";
 import { useEffect } from "react";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function Home() {
-
-  useEffect(()=>{
-    permanentRedirect('/product-inventory')
-  },[])
+  useEffect(() => {
+    permanentRedirect("/product-inventory");
+  }, []);
 
   return (
-    <main className="grid min-h-screen place-items-center p-24 text-9xl">
-      <div>LOADING...</div>
-    </main>
+    <NextUIProvider>
+      <main className="grid min-h-screen place-items-center p-24 text-9xl">
+        <div>LOADING...</div>
+      </main>
+    </NextUIProvider>
   );
 }
