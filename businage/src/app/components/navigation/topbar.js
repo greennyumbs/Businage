7b3b747cc2afbf40f58e94db891b609c484d1eb2
NGOMNGOM <87158881+NGOMNGOM@users.noreply.github.com>
2024-03-12 20:50:11 +0700

    Topbar Bug Fixing
    Adding if condition before display

--- businage/src/app/components/navigation/topbar.js
+++ businage/src/app/components/navigation/topbar.js
@@ -6,12 +6,23 @@ import { usePathname } from "next/navigation"
 export default function Topbar() {
     const pathname = usePathname()
     const words = pathname.slice(1).split("-");
-    if (words[1] == "expense") {
-        words[1] = "Expense & Sale"
+    // Debug for first visiting the page which cause no pathname exist 
+    // By checking before display the value
+    if(words.length > 1){
+        if (words[1] == "expense") {
+            words[1] = "Expense & Sale"
+        }
+        else if (words[1] == "tradein") {
+            words[1] = "Trade-In Income"
+        }
+        else{
+            words[1] = words[1][0].toUpperCase() + words[1].slice(1)
+        }
     }
-    else if (words[1] == "tradein") {
-        words[1] = "Trade-In Income"
+    else {
+        words[1] = "Loading..."
     }
+    
 
     return (
         <nav className="fixed top-0 z-10 w-screen h-14 flex gap-x-3  items-center pl-64 shadow-sm shadow-zinc-200 bg-white">
@@ -23,7 +34,7 @@ export default function Topbar() {
 
             </h1>
             <div>
-                {/* {words[1][0].toUpperCase() + words[1].slice(1)} */}
+                {words[1]}
             </div>
 
         </nav>
