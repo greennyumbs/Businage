0dc64486ca872f48bbdafea549483f474d4a0668
Sirivarakul <sirivarakuljiropakarn@gmail.com>
2024-03-12 23:14:15 +0700

    Expense form & Switch
    - Update form styling
    - Added switch for changing between expense and sale

--- businage/src/app/(monetary)/monetary-expense-sale/page.js
+++ businage/src/app/(monetary)/monetary-expense-sale/page.js
@@ -1,10 +1,18 @@
-import ExpenseForm from '@/app/_component/ExpenseForm'
+'use client'
+
+import ExpenseForm from '@/app/components/expense/ExpenseForm'
 import React from 'react'
+import {Tabs, Tab} from "@nextui-org/react";
 
 function ExpenseSale() {
 
   return (
-    <ExpenseForm/>
+    <Tabs variant="light" className="mx-10 my-5">
+      <Tab className="px-5" key="expense" title="Expense">
+        <ExpenseForm/>
+      </Tab>
+      <Tab className="px-5" key="sale" title="Sale">Sale components goes here</Tab>
+    </Tabs>
   )
 }
 
