f8b506c5922831466c4256ba1e08fcdd547360f7
greennyumbs <thanatat.sinc@hotmail.com>
2024-05-06 12:14:54 +0700

    Fix directory correction in trade_in route

--- businage/src/app/api/trade_in/route.js
+++ businage/src/app/api/trade_in/route.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-import getTradeInStock from '../utils/getTradeIn';
+import getTradeInStock from '../../utils/getTradeIn';
 
 export async function POST(req){
     const body = await req.json()
